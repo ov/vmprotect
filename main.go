1608c9617316d7252168bbec17fdd2f7846d4270
DenisMikhalev <boloden@gmail.com>
2015-12-26 23:14:41 +0300

    Add powmod

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func base10Encode(str string) (string) {
 func base10Decode(str string) (string) {
 	var data = new(big.Int)
 	if _, succ := data.SetString(str, 10); !succ {
-		fmt.Printf("Error base10Decode: %v", str)
+		fmt.Printf("Error in base10Decode: %v", str)
 	}
 
 	var res string
@@ -45,4 +45,40 @@ func base10Decode(str string) (string) {
 	}
 
 	return res
+}
+
+func powmod(_base string, _exponent string, _modulus string) (string) {
+	var base = new(big.Int)
+	if _, succ := base.SetString(_base, 10); !succ {
+		fmt.Printf("Error in powmod, can't convert _base: %v", _base)
+	}
+
+	var exponent = new(big.Int)
+	if _, succ := exponent.SetString(_exponent, 10); !succ {
+		fmt.Printf("Error in powmod, can't convert _exponent: %v", _exponent)
+	}
+
+	var modulus = new(big.Int)
+	if _, succ := modulus.SetString(_modulus, 10); !succ {
+		fmt.Printf("Error in powmod, can't convert _modulus: %v", _modulus)
+	}
+
+	var square = new(big.Int)
+	var _square = base
+	_square.DivMod(base, modulus, square)
+	var result = big.NewInt(1)
+
+	for {
+		if exponent.Cmp(big.NewInt(0)) <= 0 { break }
+		if exponent.Cmp(big.NewInt(2)) != 0 {
+			var _result = result
+			_result.DivMod(_result.Mul(_result, square), modulus, result)
+		}
+
+		var _square = square
+		_square.DivMod(_square.Mul(_square, _square), modulus, square)
+		exponent.Div(exponent, big.NewInt(2))
+	}
+
+	return result.String()
 }
